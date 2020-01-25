6e86b86f44c489abb0cea22398ef83cf0b24fba0
Lup Gabriel <gwolf2u.website@gmail.com>
2020-01-25 17:22:11 +0200

    Add Cocon and Quando fonts [1/2]
    Change-Id: I6396811a1e93a7cbb8b520ddcd1cc461f80903e3

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -68,6 +68,8 @@ LOCAL_REQUIRED_MODULES := \
 	FontFifa2018SourceOverlay \
 	Font20SevenSourceOverlay \
 	FontRoadRageSourceOverlay \
+	FontCoconSourceOverlay \
+	FontQuandoSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
